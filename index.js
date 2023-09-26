c6fd3a50b0ffbf8099700624a191418fa48b2c09
aent <alintssr@gmail.com>
2023-09-26 12:27:59 -0400

    strict equality

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-for (i = 1; i <= 100; i++){                 // iterates over the numbers from 1 to 100 inclusive
-    if((i % 3 == 0) && (i % 5 == 0)){       // console.log's FizzBuzz for numbers that are both multiples of 3 and 5     
+for (i = 1; i <= 100; i++){                    // iterates over the numbers from 1 to 100 inclusive
+    if((i % 3 === 0) && (i % 5 === 0)){        // console.log's FizzBuzz for numbers that are both multiples of 3 and 5     
         console.log("FizzBuzz");            
-    }else if (i % 3 == 0){                  // console.log's Fizz for multiples of 3
+    }else if (i % 3 === 0){                    // console.log's Fizz for multiples of 3
         console.log("Fizz");
-    }else if (i % 5 == 0){                  // console.log's Buzz for multiples of 5
+    }else if (i % 5 === 0){                    // console.log's Buzz for multiples of 5
         console.log("Buzz");
-    }else{                                  // console.log's the rest of the numbers from 1 to 100
+    }else{                                     // console.log's the rest of the numbers from 1 to 100
         console.log(i);
     }
 }
